6971e50657522881f8e8fc11e455a55f47b07760
Sofia <jonssonsofia93@gmail.com>
2019-04-26 14:09:40 -0700

    Got toys to render in cards on page

--- src/index.js
+++ src/index.js
@@ -5,12 +5,43 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // YOUR CODE HERE
   function getToyCollection(){
-    let toyCollectionDiv = document.getElementById('toy-collection')
       fetch("http://localhost:3000/toys")
         .then(res => res.json())
-        .then(json=>console.log(json))
+        .then(json=> createCardDiv(json))
     }
 
+  function createCardDiv(toys){
+    toys.forEach(toy => {
+      createToy(toy)
+    })
+  }
+
+  function createToy(toy){
+    let toyCollectionDiv = document.getElementById('toy-collection')
+
+    let toyCardDiv= document.createElement('div')
+    toyCardDiv.classList.add('card')
+
+    let toyName = document.createElement('h2')
+    toyName.textContent = toy.name
+
+    let toyImage = document.createElement('img')
+    toyImage.classList.add("toy-avatar")
+    toyImage.src = toy.image
+
+    let toyLikes = document.createElement('p')
+    toyLikes.textContent = `${toy.likes} Likes`
+
+    let likeButton = document.createElement('button')
+    likeButton.classList.add('like-btn')
+    likeButton.textContent = 'Like'
+
+    toyCardDiv.appendChild(toyName)
+    toyCardDiv.appendChild(toyImage)
+    toyCardDiv.appendChild(toyLikes)
+    toyCardDiv.appendChild(likeButton)
+    toyCollectionDiv.appendChild(toyCardDiv)
+  }
 
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
