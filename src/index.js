e8e60b83a36469125869e42f37591e1137fd7c13
Sofia <jonssonsofia93@gmail.com>
2019-04-26 13:47:34 -0700

    Got toys to render

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+document.addEventListener("DOMContentLoaded", function() {
+  const addBtn = document.querySelector('#new-toy-btn')
+  const toyForm = document.querySelector('.container')
+  let addToy = false
 
-// YOUR CODE HERE
+  // YOUR CODE HERE
+  function getToyCollection(){
+    let toyCollectionDiv = document.getElementById('toy-collection')
+      fetch("http://localhost:3000/toys")
+        .then(res => res.json())
+        .then(json=>console.log(json))
+    }
 
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-    // submit listener here
-  } else {
-    toyForm.style.display = 'none'
-  }
-})
+
+  addBtn.addEventListener('click', () => {
+    // hide & seek with the form
+    addToy = !addToy
+    if (addToy) {
+      toyForm.style.display = 'block'
+      // submit listener here
+    } else {
+      toyForm.style.display = 'none'
+    }
+  })
 
 
 // OR HERE!
+
+
+
+
+
+
+function main(){
+getToyCollection()
+
+}
+
+main()
+
+console.log("The DOM has loaded");
+});
